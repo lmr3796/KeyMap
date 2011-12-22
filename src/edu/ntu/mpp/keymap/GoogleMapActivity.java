2181586d7204da59dff859cd92683a4a2e75f206
ilmftb3191 <ilmftb3191@hotmail.com>
2011-12-22 17:54:16 +0800

    Modified

--- src/edu/ntu/mpp/keymap/GoogleMapActivity.java
+++ src/edu/ntu/mpp/keymap/GoogleMapActivity.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Intent;
 import android.graphics.Canvas;
@@ -77,7 +78,7 @@ public class GoogleMapActivity extends MapActivity implements Runnable{
         status = (TextView)findViewById(R.id.info);
         //
         refresh.setOnTouchListener(new Button.OnTouchListener(){
-            @Override
+           
            public boolean onTouch(View arg0, MotionEvent motionEvent) {
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {  //���U���ɭԧ��ܭI�����C��
                 	refresh.setBackgroundResource(R.drawable.re_on);
@@ -90,7 +91,7 @@ public class GoogleMapActivity extends MapActivity implements Runnable{
         });
         
         checkin.setOnTouchListener(new Button.OnTouchListener(){
-            @Override
+           
            public boolean onTouch(View arg0, MotionEvent motionEvent) {
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {  //���U���ɭԧ��ܭI�����C��
                 	checkin.setBackgroundResource(R.drawable.pa_on);
@@ -200,23 +201,86 @@ public class GoogleMapActivity extends MapActivity implements Runnable{
 			break;
 		}
 	}
-	@Override
+	
 	public void run() {
 		//ProgressBar progress = new ProgressBar(this);
+		JSONArray places;
+		JSONObject location;
+		JSONArray keyword;
+		ArrayList<String> checkins;
+		String allcheckin;
 		
+		FacebookMiner fMiner = new FacebookMiner(KeyMap.facebook);
+		YahooSplitter splitter = new YahooSplitter();
+		
+		places = fMiner.getPlaceID(intent.getDoubleExtra("lat", 0), intent.getDoubleExtra("lng", 0));
+		int [] check = new int[places.length()];
+		Log.e("places",places.toString());
+		
+		for(int i = 0 ; i < check.length ; i++)
+			check[i] = 0;
+		
+		for(int i = 0 ; i < places.length() ; i++){
+			if(check[i] == 1)
+				continue;
+			
+			try{
+				location = (JSONObject) places.get(i);
+				allcheckin = fMiner.getAllCheckins(location.getString("id"));
+			
+				if(allcheckin.isEmpty()){
+					check[i] = 1;
+					continue;
+				}
+				for(int j = i+1 ; j < places.length() ; j++){
+					JSONObject near = (JSONObject) places.get(j);
+					if(distance.dist(location.getDouble("lat"),location.getDouble("lng"), near.getDouble("lat"), near.getDouble("lng")) < 20){
+						//allcheckin += fMiner.mergeCheckins(fMiner.getCheckins(near.getString("id")));
+						allcheckin += fMiner.getAllCheckins(near.getString("id"));
+						check[j] = 1;
+						Log.e("merge",location.getString("name") + " " + near.getString("name"));
+					}
+				}
+				keyword = splitter.split(allcheckin);
+				if(keyword != null){
+					JSONObject keycloud = new JSONObject();
+					keycloud.put("name", location.get("name"));
+					keycloud.put("id", location.get("id"));
+					keycloud.put("lat", location.get("lat"));
+					keycloud.put("lng", location.get("lng"));
+					keycloud.put("kw", keyword);
+					//Log.e("allcheckin",allcheckin);
+					//Log.e("keyword",keyword.toString());
+					result.put(keycloud);
+				}
+			}catch(JSONException e){
+				Log.e("ilmftb3191",e.getStackTrace().toString());
+				continue;
+			}
+		}
+		
+		/*
         FacebookMiner miner = new FacebookMiner(KeyMap.facebook);
         Splitter yahoo = new YahooSplitter();
         JSONArray places = miner.getPlaceID(intent.getDoubleExtra("lat", 0), intent.getDoubleExtra("lng", 0));
+        Log.e("lmr3796", places.toString());
         for(int i = 0 ; i < places.length() ; i++){
         	try{
-        		String page_id = places.getJSONObject(i).getString("id");
-        		String ckin = miner.getAllCheckins(page_id).trim();
-        		result=yahoo.split(ckin);
+        		JSONObject place = places.getJSONObject(i);
+        		String ckin = miner.getAllCheckins(place.getString("id")).trim();
+        		result=(yahoo.split(ckin));
+        		if(result == null){
+        			pl
+        			continue;
+        		}else{
+        			
+        		}
         		Log.e("result", result.toString());
         	}catch (JSONException e){
         		continue;
         	}
         }
+        */
 		/*
         Server server = new Server();
         try{
